6a62af6e7e7c2bd989d9876a5f12c1fe700a5bb8
abdulwasaemomin <mominwasae8@gmail.com>
2020-02-25 01:07:17 +0530

    First mocha test

---
+++ index.test.js
@@ -0,0 +1,11 @@
+let log=console.log
+let expect=require('chai').expect
+let should=require('chai').should()
+let _=require('lodash')
+
+
+describe("#mocha basics",()=>{
+    it("true should be true",()=>{
+        true.should.be.true
+    })
+})
